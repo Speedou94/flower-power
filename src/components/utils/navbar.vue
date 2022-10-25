ad57f2077662c0ec232d99925566b7addad30802
abdzk <102174098+abdzk@users.noreply.github.com>
2022-10-25 22:31:22 +0200

    Mise en page de la navbar

--- src/components/utils/navbar.vue
+++ src/components/utils/navbar.vue
@@ -1,10 +1,8 @@
 <template>
   <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-      <a class="navbar-item" href="https://bulma.io">
-        <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
-      </a>
-
+      <button class="button">Se connecter</button>
+      <button class="button">S'inscrire</button>
       <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
@@ -12,61 +10,33 @@
       </a>
     </div>
 
-    <div id="navbarBasicExample" class="navbar-menu">
-      <div class="navbar-start">
-        <a class="navbar-item">
-          Home
-        </a>
 
-        <a class="navbar-item">
-          Documentation
-        </a>
-
-        <div class="navbar-item has-dropdown is-hoverable">
-          <a class="navbar-link">
-            More
-          </a>
-
-          <div class="navbar-dropdown">
-            <a class="navbar-item">
-              About
-            </a>
-            <a class="navbar-item">
-              Jobs
-            </a>
-            <a class="navbar-item">
-              Contact
-            </a>
-            <hr class="navbar-divider">
-            <a class="navbar-item">
-              Report an issue
-            </a>
-          </div>
-        </div>
-      </div>
-
-      <div class="navbar-end">
-        <div class="navbar-item">
-          <div class="buttons">
-            <a class="button is-primary">
-              <strong>Sign up</strong>
-            </a>
-            <a class="button is-light">
-              Log in
-            </a>
-          </div>
-        </div>
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <button class="button">Eshop</button>
       </div>
     </div>
+
   </nav>
 </template>
 
 <script>
 export default {
   name: "navbar"
-}
+};
 </script>
 
 <style scoped>
+.navbar {
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: flex-end;
+  background: linear-gradient(violet, yellow);
+}
+
+.button {
+  margin: 3px;
+}
+
 
 </style>
