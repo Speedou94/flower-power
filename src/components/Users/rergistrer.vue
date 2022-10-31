ed2cdb9a8cbbabcebfa9295564766408b52f6cea
abdzk <102174098+abdzk@users.noreply.github.com>
2022-10-31 16:18:10 +0100

    commit

---
+++ src/components/Users/rergistrer.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "rergistrer"
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
-    <h1>Hola</h1>
+    <h1>liste article
+    </h1>
   </div>
 </template>
